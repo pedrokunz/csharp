540006ed1bdae8ed11d63fa75b7c005d969ecc65
Pedro Kunz <pedro.kunz@gmail.com>
2021-10-05 20:57:48 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length <= 0)
+            {
+                Console.WriteLine("Nenhum parâmetro informado.");
+                return;
+            }
+
+            string funcao = args[0];
+
+            switch (funcao)
+            {
+                case "soma":
+                    soma(args);
+                    break;
+                default:
+                    Console.WriteLine("Função informada não existe.");
+                    break;
+            }
+        }
+
+        static void soma(string[] args)
+        {
+            int resultado = 0;
+            for (short i = 1; i < args.Length; i++)
+            {
+                resultado += Convert.ToInt32(args[i]);
+            }
+
+            Console.WriteLine("O resultado da soma é: {0}", resultado);
+        }
+    }
+}
