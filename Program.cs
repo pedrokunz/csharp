2531e82d40ecbe6685192894750cec1fd1ad6ad1
Murilo Nascimento <murilo_gyncity@hotmail.com>
2021-10-07 20:32:38 -0300

    função subtrai incluida

--- Program.cs
+++ Program.cs
@@ -19,9 +19,14 @@ namespace csharp
                 case "soma":
                     soma(args);
                     break;
+
+                case "subtracao":
+                    subtrai(args);
+                    break;
                 default:
                     Console.WriteLine("Função informada não existe.");
                     break;
+
             }
         }
 
@@ -35,5 +40,18 @@ namespace csharp
 
             Console.WriteLine("O resultado da soma é: {0}", resultado);
         }
+
+        static void subtrai(string[] args)
+        {
+            int resultado = 0;
+            for (short i = 1; i < args.Length; i++)
+            {
+                resultado -= Convert.ToInt32(args[i]);
+            }
+
+            Console.WriteLine("O resultado da subtração é: {0}", resultado);
+        }
     }
 }
+
+
