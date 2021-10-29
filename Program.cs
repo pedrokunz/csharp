22254cd4316b5dbacb8d1d7dd39f19a9ff1fcc01
Pedro Kunz <pedro.kunz@gmail.com>
2021-10-28 21:12:31 -0300

    new study cases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace csharp
 {
@@ -24,6 +27,12 @@ namespace csharp
                     break;
                 case "multiplica":
                     multiplica(args);
+                    break;
+                case "parOuImpar":
+                    parOuImpar(args);
+                    break;
+                case "fritePastel":
+                    fritePastel(args);
                     break;
                 default:
                     Console.WriteLine("Função informada não existe.");
@@ -63,5 +72,69 @@ namespace csharp
 
             Console.WriteLine("O resultado da multiplicação é: {0}", resultado);
         }
+
+        static void parOuImpar(string[] args) {
+            var valor = Convert.ToDouble(args[1]);
+            string resultado;
+            var resultadoDaDivisaoPorDois = valor % 2;
+            if (resultadoDaDivisaoPorDois == 0) {
+                resultado = "par";
+            } else {
+                resultado = "ímpar";
+            }
+
+            Console.WriteLine("O resultado é: {0}", resultado);
+        }
+    
+        static void fritePastel(string[] args) {
+            string sabor = args[1];
+            string tamanho = args[2];
+            Pastel pastel = new Pastel(sabor, tamanho);
+
+            recebaPedido();
+            montePastel(pastel);
+            preparePastel(pastel);
+            entreguePastel(pastel);
+        }
+
+        static void recebaPedido() {
+            Console.WriteLine("Pedido recebido");
+            Thread.Sleep(500);
+        }
+
+        static void montePastel(Pastel pastel) {
+            Console.WriteLine("Montando um pastel {0} de {1}", pastel.Tamanho, pastel.Sabor);
+            Thread.Sleep(1000);
+        }
+
+        static void preparePastel(Pastel pastel) {
+            Console.WriteLine("Fritando o pastel de {0}", pastel.Sabor);
+            Thread.Sleep(1000);
+        }
+
+        static void entreguePastel(Pastel pastel) {
+            Console.WriteLine("Entregando o pastel de {0}", pastel.Sabor);
+            Thread.Sleep(1000);
+        }
+    }
+
+    enum SaborEnum {
+        Carne,
+        Frango
+    }
+
+    enum TamanhoEnum {
+        P, M, G
+    }
+
+    class Pastel {
+        public Pastel(string sabor, string tamanho)
+        {
+            this.Sabor = sabor;
+            this.Tamanho = tamanho;
+        }
+
+        public string Sabor {get;set;}
+        public string Tamanho {get;set;}
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# csharp
-Introdução ao C#
+# Pensamento computacional
+> Fonte: [Escolas Disruptivas](https://escolasdisruptivas.com.br/metodologias-inovadoras/pensamento-computacional/)
+
+## 1. Decomposição
+Corresponde à habilidade de dividir um problema complexo em partes menores. Trabalhar um fragmento do problema por vez facilita a solução desse problema, permitindo ainda maior atenção a cada etapa.
+
+O desenvolvimento dessa competência é perceptível no comportamento dos estudantes. A decomposição ajuda a diminuir a ansiedade e o medo frente aos desafios. Os alunos conseguem encontrar as respostas de cada parte do problema com mais confiança e rapidez, enquanto seguem um passo a passo para a solução da grande questão.
+
+## 2. Abstração
+Essa habilidade propõe o foco em processos relevantes em vez de priorizar os detalhes, de modo que a solução possa ser válida para outros problemas.
+
+Ao filtrar e classificar os dados mais relevantes para a resolução das questões, os alunos são capazes de desenvolver uma análise mais crítica e atenta à essência do tema trabalhado.
+
+## 3. Reconhecimento de padrões
+Para identificar tendências de comportamento é necessário observar atentamente a questão, reconhecendo padrões e similaridades.
+
+Essa competência permite a construção de soluções para problemas comuns de forma inovadora, desenvolvendo o espírito empreendedor nos estudantes.
+
+## 4. Pensamento algorítmico
+Ainda que a palavra algoritmo seja mais utilizada no contexto computacional, esse pensamento corresponde à criação de passos e soluções para alcançar um objetivo específico para qualquer problema, de ordem matemática ou não.
+
+Desenvolver essa competência nos alunos estimula a criatividade, a fim de criar soluções cada vez mais eficazes.
